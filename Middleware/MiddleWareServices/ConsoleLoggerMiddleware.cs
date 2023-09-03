51335b29b5ad84628767bad16c5bdd2ad70b7dc1
MrIzzat <guineapigsarecute3748@gmail.com>
2023-09-03 13:30:36 +0300

    Updated Middleware Project

---
+++ Middleware/MiddleWareServices/ConsoleLoggerMiddleware.cs
@@ -0,0 +1,18 @@
+﻿namespace Middleware.MiddleWareServices
+{
+    public class ConsoleLoggerMiddleware :IMiddleware //This middleware needs to be injected into the services in the program.cs fiel
+    {// services can be injected directly into this custom middleware
+     //The other way to make a middleware is by using conventional middlewares
+     //as seen here https://learn.microsoft.com/en-us/aspnet/core/fundamentals/middleware/extensibility?view=aspnetcore-7.0#imiddleware
+     //There are differnces between using the interface vs using by convetion which can be seen in the link above
+
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next) 
+        {
+            Console.WriteLine("Hell yeah Before Request");
+            await next(context);//This will call the next middleware in the pipeline
+            Console.WriteLine("Hell yeah After request");//This line of code will only be triggered on the way back
+        }
+
+        
+    }
+}
--- Middleware/Program.cs
+++ Middleware/Program.cs
@@ -10,26 +10,90 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddTransient<CustomMiddleWare>();
+builder.Services.AddTransient<ConsoleLoggerMiddleware>();//To inject the middlewares
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-//These are system defined middlewares (except for app.Run())
+// Configure the HTTP request pipeline. This is like the Configure() method in the startup class
+
+//In order to access the middlewares with the SwaggerUI the middleware code should be added before the if statment
 if (app.Environment.IsDevelopment())//checks the environment to see if it's a development (?)
 {
-    app.UseClassWithNoImplementationMiddleware(); 
+    
     app.UseSwagger();
     app.UseSwaggerUI();
+    //app.UseClassWithNoImplementationMiddleware();//this should be after the swagger and swaggerUI 
+    //There is a better method than this so that static classes don't need to be used
 }
-/*app.Run(async context =>
+
+//To chain delegates Use() should be used
+/*app.Use(async (context, next) =>
 {
-    await context.Response.WriteAsync("Hell yeah");
+    Console.WriteLine("Hell yeah Before Request");
+    await next();//This will call the next middleware in the pipeline
+    Console.WriteLine("Hell yeah After request");
+});*/
+//In order to create a custum Use() middleware that acts as the end of the middlieware pipeline instead of run()
+//just add _ => and remove the next like this
+/*app.Use( _ => (async context => 
+{
+    Console.WriteLine("Hell yeah From Custom Final Middleware");
+    await context.Response.WriteAsync("Hell yeah From Custom Final Middleware");
 
-})*/;//in async with app.Run()
+}));*/
+app.Map("/favicon.ico", (app) => { });
+//This can be used instead of inline middleware after setting it up 
+app.UseMiddleware<ConsoleLoggerMiddleware>();
+
+
+//.Map() is a convention that is used to indicate that the middleware pipeline is being branched
+//It completely breaks off the pipeline of the middlewares anything after the middleware in the original branch will not be invoked
+
+app.Map("/map",  app => //This needs to be called app
+{
+    app.Run(async (context) =>
+    {
+        Console.WriteLine("hell yeah Map method");
+        await context.Response.WriteAsync("hell yeah from Map method");
+    });
+}); //This will not go to the Run() middleware as it has completely branched off to the middlewares within the app => method
+
+app.MapWhen(context => context.Request.Query.ContainsKey("q"),app =>{
+    app.Run( async (context) =>
+    {
+        await context.Response.WriteAsync("Conditional Hell yeah");
+    });
+}); //This will check if the request query has a q in it
+//if it does then it will go to the middlewares specified in the condition
+// app.UseWhen() can be used instead of MapWhen() But the difference is the UseWhen() will go back into the pipeline while MapWhen() does not
+//
+
+
+//What's being passed to the app.Run() method is a delegate method to handle middleware services
+app.Run(async context =>
+{
+    Console.WriteLine("Hell yeah from run");
+    await context.Response.WriteAsync("Hell yeah from run");//This writes hell yeah into the response
+
+});//in async with app.Run()
 //when opening the website from the link without the extra stuff in the url it will print hell yeah
-//In application this is done in a seperate class
+//In application this is done in a seperate
+//this is known as an inline middleware since it's done inside the program.cs class
+//middlewares can also be called request delegate
+//The run delegate receives only a context parameter which means it doesn't know about the next middleware
+//These are usually called terminal delegates because it usually ends the middleware pipeline
+//It is more of a convention to use run to indicate the end of the pipeline a
+
+app.Run(async context =>
+{
+    
+    await context.Response.WriteAsync("Hell yeah for second time from run");//This writes hell yeah into the response
+
+});
+//This one will not be called as the run middleware before it has terminated the pipeline
 
 
+//These are system defined middlewares (includding the swaggerUI() methods)
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
